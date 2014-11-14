e9f8051b9892934e2fceddc67b67d4e972d0f359
Benjamin Toll <benjam72@yahoo.com>
2014-11-14 12:44:49 -0500

    Add type attribute to bfind and added a TODO

--- bfind.sh
+++ bfind.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# TODO: add support for exec, i.e.:
+#       -exec grep -rn interceptTitles {} \;
 
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <dir_name> <file_name>"
     exit 1
 fi
-vim -p `find "$1" -name "$2"`
+vim -p `find "$1" -type f -name "$2"`
