25b2abae323ca79f29d6151cbbbd72d43c300905
Benjamin Toll <benjam72@yahoo.com>
2015-01-24 18:47:12 -0500

    Updated the update_bootstrap script with the alias to use intead of the script.

--- sencha/update_bootstrap.sh
+++ sencha/update_bootstrap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Note it's really better to use an alias than a script.
+# For example:
+
+#     alias bootstrap='pushd $SDK5/ext; sencha ant bootstrap; popd'
+#
+
 BOOTSTRAP_DIR=${BOOTSTRAP_LOCATION:-/usr/local/www/SDK5/ext/}
 
 cd $BOOTSTRAP_DIR
