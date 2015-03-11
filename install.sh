203be162874d6ba421b4f1cde1191761528f854d
Benjamin Toll <benjam72@yahoo.com>
2015-03-11 13:02:35 -0500

    fixed bugs in installer

--- install.sh
+++ install.sh
@@ -2,17 +2,15 @@
 # Installation script for custom CLI tools.
 
 echo "[Install] Creating symbolic links for utils..."
-echo
-pushd utils
+
 ln -s "$PWD"/bootstrap.sh /usr/local/bin/bootstrap
 ln -s "$PWD"/check-dependencies.sh /usr/local/bin/check-dependencies
-ln -s "$PWD"/get-fiddle.sh /usr/local/bin/get-fiddle
+ln -s "$PWD"/get_fiddle.sh /usr/local/bin/get_fiddle
 ln -s "$PWD"/make_file.sh /usr/local/bin/make_file
 ln -s "$PWD"/make_ticket.sh /usr/local/bin/make_ticket
 
 # https://github.com/btoll/utils/tree/master/tmux
 ln -s "$PWD"/tmux/tmuxly.sh /usr/local/bin/tmuxly
-popd
 
 echo
 echo "[Install] Installation complete."
