ad0764aefd9371773dc94c05fa8873ece70e63e1
Benjamin Toll <benjam72@yahoo.com>
2015-03-21 23:31:31 -0400

    Cleaned up root dir of repo

--- install.sh
+++ install.sh
@@ -3,11 +3,11 @@
 
 echo "[Install] Creating symbolic links for utils..."
 
-ln -s "$PWD"/bootstrap.sh /usr/local/bin/bootstrap
-ln -s "$PWD"/check-dependencies.sh /usr/local/bin/check-dependencies
-ln -s "$PWD"/get_fiddle.sh /usr/local/bin/get_fiddle
-ln -s "$PWD"/make_file.sh /usr/local/bin/make_file
-ln -s "$PWD"/make_ticket.sh /usr/local/bin/make_ticket
+ln -s "$PWD"/bash/bootstrap.sh /usr/local/bin/bootstrap
+ln -s "$PWD"/bash/check-dependencies.sh /usr/local/bin/check-dependencies
+ln -s "$PWD"/bash/get_fiddle.sh /usr/local/bin/get_fiddle
+ln -s "$PWD"/bash/make_file.sh /usr/local/bin/make_file
+ln -s "$PWD"/bash/make_ticket.sh /usr/local/bin/make_ticket
 
 # https://github.com/btoll/utils/tree/master/tmux
 ln -s "$PWD"/tmux/tmuxly.sh /usr/local/bin/tmuxly
