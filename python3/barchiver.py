4f8d4c6a3e678dfb13c08e65514b903574d4b210
Benjamin Toll <benjam72@yahoo.com>
2015-03-22 21:18:19 -0400

    Improving barchiver

--- python3/barchiver.py
+++ python3/barchiver.py
@@ -1,51 +1,47 @@
-import sys, json
-from socket import gethostname
-from time import localtime
-from os import getcwd, makedirs, waitpid, path, remove
-from shutil import make_archive, move, ignore_patterns
-from getopt import getopt, GetoptError
-from subprocess import Popen
-from getpass import getuser
+import getopt, getpass, json, os, re, shutil, socket, subprocess, sys, time
 
 def usage(level):
-    if level == 0:
-        print('Usage: python3 tar_jslite.py [-h | --help [[-n | --name] [-s | --src] [-d | --dest] [-r | --root] [-c | --config] [--silent]]]\nTry `python3 barchiver.py --help` for more information.')
-
-    elif level == 1:
+#    if level == 0:
+#        print('Usage: python3 tar_jslite.py [-h | --help [[-n | --name] [-s | --src] [-d | --dest] [-r | --root] [-c | --config] [--silent]]]\nTry `python3 barchiver.py --help` for more information.')
+#
+#    elif level == 1:
+     if level == 1:
         print('''Usage:
 
-  Optional flags:
-  -c, --config  A config file that the script will read to get remote system information. Session will be non-interactive. Useful for automation.
-  -d, --dest    The location of where the assets should be archived. Defaults to cwd.
-  -n, --name    An optional archive name. The default is YYYYMMDDHHMMSS.
-  -r, --root    The directory that will be the root directory of the archive. For example, we typically chdir into root_dir before creating the archive. Defaults to '.'
-  -s, --src     The location of the assets to archive. Defaults to cwd.
-  -h, --help    Help.''')
+Optional flags:
+    -c, -config, --config      A config file that the script will read to get remote system information. Session will be non-interactive. Useful for automation.
+    -d, -dest, --dest          The location of where the assets should be archived. Defaults to YYYYMMDDHHMMSS.
+    -n, -name, --name          An optional archive name. The default is YYYYMMDDHHMMSS.
+    -p, -pattern, --pattern
+    -r, -root, --root          The directory that will be the root directory of the archive. For example, we typically chdir into root_dir before creating the archive. Defaults to '.'
+    -s, -src, --src            The location of the assets to archive. Defaults to cwd.
+    -h, -help, --help          Help.''')
 
 def pushToServer():
     print('Pushing to server...')
-    p = Popen(['scp', '-P', port, dest_dir + '/' + tarball, username + '@' + hostname + ':' + dest_remote])
-    sts = waitpid(p.pid, 0)
-    print('Archive ' + tarball + ' pushed to ' + dest_remote + ' on remote server.')
+    p = subprocess.Popen(['scp', '-P', port, dest_dir + '/' + archive, username + '@' + hostname + ':' + dest_remote])
+    sts = os.waitpid(p.pid, 0)
+    print('Archive ' + archive + ' pushed to ' + dest_remote + ' on remote server.')
 
 def main(argv):
     try:
-        opts, args = getopt(argv, 'hn:s:d:r:c:', ['help', 'silent', 'name=', 'src=', 'dest=', 'root=', 'config='])
-    except GetoptError:
+        opts, args = getopt.getopt(argv, 'hn:p:s:d:r:c:', ['help', 'silent', 'name=', 'pattern=', 'src=', 'dest=', 'root=', 'config='])
+    except getopt.GetoptError:
         usage(0)
         sys.exit(2)
 
     # Define some variables.
-    dest_dir = '.'
+    archive = ''
     src_dir = '.'
     root_dir = '.'
     silent = False
-    today = localtime()
-    tmp_name = str(today.tm_year) + str(today.tm_mon) + str(today.tm_mday) + str(today.tm_hour) + str(today.tm_min) + str(today.tm_sec)
+    pattern = '^.*$'
+    today = time.localtime()
+    dest_dir = tmp_name = str(today.tm_year) + str(today.tm_mon) + str(today.tm_mday) + str(today.tm_hour) + str(today.tm_min) + str(today.tm_sec)
     port = '22'
     dest_remote = '~'
-    hostname = gethostname()
-    username = getuser()
+    hostname = socket.gethostname()
+    username = getpass.getuser()
     format = 'gztar'
     formats = {
         'gztar': '.tar.gz',
@@ -55,18 +51,20 @@ def main(argv):
     }
 
     for opt, arg in opts:
-        if opt in ('-h', '--help'):
+        if opt in ('-h', '-help', '--help'):
             usage(1)
             sys.exit(0)
-        elif opt in ('-n', '--name'):
+        elif opt in ('-n', '-name', '--name'):
             tmp_name = arg
-        elif opt in ('-s', '--src'):
+        elif opt in ('-s', '-src', '--src'):
             src_dir = arg
-        elif opt in ('-d', '--dest'):
+        elif opt in ('-d', '-dest', '--dest'):
             dest_dir = arg
-        elif opt in ('-r', '--root'):
+        elif opt in ('-p', '-pattern', '--pattern'):
+            pattern = arg
+        elif opt in ('-r', '-root', '--root'):
             root_dir = arg
-        elif opt in ('-c', '--config'):
+        elif opt in ('-c', '-config', '--config'):
             try:
                 # This can read 'username hostname port'.
                 # username, hostname, port = open(arg, encoding='utf-8').readline().split()
@@ -87,54 +85,47 @@ def main(argv):
                 print(e)
                 sys.exit(1)
 
-    if not silent:
-        resp = input('''Choose an archive format:
-    0 = tar.gz
-    1 = tar.bz2
-    2 = tar
-    3 = zip
+    try:
+        if not silent:
+            resp = input('''Choose an archive format:
+    0 = tar.bz2
+    1 = tar.gz
+    2 = zip
+    3 = tar
     ? [0]: ''')
-        if resp in ['1', '2', '3']:
-            if resp == '1':
+            if resp in ['1', '2', '3']:
+                if resp == '1':
+                    format = 'gztar'
+
+                if resp == '2':
+                    format = 'zip'
+
+                if resp == '3':
+                    format = 'tar'
+            else:
                 format = 'bztar'
 
-            if resp == '2':
-                format = 'tar'
-
-            if resp == '3':
-                format = 'zip'
-        else:
-            format = 'gztar'
-
-    tarball = tmp_name + formats[format]
-
-    try:
-        # Create the destination directory if it doesn't exist.
-        if not path.exists(dest_dir):
-            makedirs(dest_dir)
-
-        try:
-            archive = make_archive(tmp_name, format, root_dir, src_dir)
-        except FileNotFoundError as e:
-            print(e)
-
-            if (path.isfile(tarball)):
-                remove(tarball)
-                print('Cleaning up...')
-
-            sys.exit(1)
-
-        # Don't move the archive if the destination directory is the cwd.
-        if dest_dir != '.' and dest_dir != getcwd():
             try:
-                move(archive, dest_dir)
-            except OSError as e:
+                # Create the destination directory if it doesn't exist.
+                if not os.path.exists(dest_dir):
+                    os.makedirs(dest_dir)
+
+                #[shutil.move(f, dest_dir) for f in next(os.walk(src_dir))[1] + next(os.walk(src_dir))[2] if re.match(pattern, f)]
+                [shutil.move(f, dest_dir) for f in os.listdir(src_dir) if re.match(pattern, f)]
+#                print(pattern)
+#                files = [f for f in os.listdir(src_dir) if re.match(pattern, f)]
+#                print(files)
+#                return;
+                archive = shutil.make_archive(tmp_name, format, root_dir, src_dir)
+            except FileNotFoundError as e:
                 print(e)
-                sys.exit(1)
 
-        print('Created new archive ' + tarball + ' in ' + path.abspath(dest_dir) + '.')
+                if (os.path.isfile(archive)):
+                    os.remove(archive)
+                    print('Cleaning up...')
 
-        if not silent:
+            print('Created new archive in ' + os.path.abspath(dest_dir) + '.')
+
             resp = input('Push to remote server? [y|N]: ')
 
             if resp in ['Y', 'y']:
@@ -155,8 +146,9 @@ def main(argv):
                     dest_remote = resp
 
                 pushToServer()
+
         else:
-                pushToServer()
+            pushToServer()
 
         print('Done!')
 
@@ -165,8 +157,8 @@ def main(argv):
         print('\nProcess aborted!')
 
         # If aborted at input() then tmp_name would have already been removed so first check for its existence.
-        if (path.isfile(tarball)):
-            remove(tarball)
+        if (os.path.isfile(archive)):
+            os.remove(archive)
             print('Cleaning up...')
 
         sys.exit(1)
