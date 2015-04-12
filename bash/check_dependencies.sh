d4bfb797b2f77307dc3367500691ebbbb0107fa0
Benjamin Toll <benjam72@yahoo.com>
2015-04-11 23:59:30 -0400

    Updated check_dependencies

--- bash/check_dependencies.sh
+++ bash/check_dependencies.sh
@@ -25,12 +25,12 @@ for n in ${!DEPENDENCIES[*]}; do
     DEPENDENCY=${DEPENDENCIES[n]}
 
     which $DEPENDENCY > /dev/null || {
-        FAILED_DEPENDENCIES+="\nName: $DEPENDENCY\nPackage: ${PACKAGES[n]}\n"
+        FAILED_DEPENDENCIES+="\n$(tput smul)Name:$(tput sgr0) $DEPENDENCY\n$(tput smul)Package:$(tput sgr0) ${PACKAGES[n]}\n"
     }
 done
 
 if [ -n "$FAILED_DEPENDENCIES" ]; then
-    echo -e "\n$(tput setaf 3)[WARNING]$(tput sgr0) This script has several dependencies that are not present on your system."
+    echo -e "\n$(tput setaf 3)[WARNING]$(tput sgr0) This script has dependencies that are not present on your system."
     echo "Please install the following:"
     echo -e $FAILED_DEPENDENCIES
     touch /tmp/failed_dependencies
