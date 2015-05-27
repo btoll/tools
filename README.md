211b6ad62f1c21c5bac90c2fde8d642e09519e78
Benjamin Toll <benjam72@yahoo.com>
2015-05-26 22:49:46 -0400

    Improved install.sh to automatically append export statement to Bash login scripts, if found.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 1. Make sure that you have write permissions to `/usr/local/bin`
 2. Clone the repo
 3. Run `./install.sh`
-4. Add the following to `.bashrc` (or similar) to let Python know how to find the build modules:
+4. If not running Bash, add the following to one of your shell's login scripts to let Python know how to find the build modules:
     - `export PYTHONPATH=$PYTHONPATH:/usr/local/bin/`
 5. Done
 
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Installation script for custom CLI tools.
 
-echo "$(tput setaf 2)[INFO]$(tput sgr0) Creating symbolic links for tools..."
+echo "$(tput setaf 2)[INFO]$(tput sgr0) Installing workflow tools..."
 
 ln -s "$PWD"/bash/bootstrap.sh /usr/local/bin/bootstrap
 ln -s "$PWD"/bash/check-dependencies.sh /usr/local/bin/check-dependencies
@@ -11,12 +11,23 @@ ln -s "$PWD"/bash/make_file.sh /usr/local/bin/make_file
 ln -s "$PWD"/bash/make_ticket.sh /usr/local/bin/make_ticket
 ln -s "$PWD"/tmux/tmuxly.sh /usr/local/bin/tmuxly
 
-# Python3 build compression tools.
+echo "$(tput setaf 2)[INFO]$(tput sgr0) Installing Python build tools..."
+
 ln -s "$PWD"/python3/server.py /usr/local/bin/server.py
 ln -s "$PWD"/python3/build/css_compress.py /usr/local/bin/css_compress.py
 ln -s "$PWD"/python3/build/js_compress.py /usr/local/bin/js_compress.py
-export PYTHONPATH=$PYTHONPATH:/usr/local/bin/
-echo -e "The location of the Python3 build tools has been added to your PYTHONPATH for this terminal session.\nHowever, it's highly recommended to put the following in your .bashrc:\n\n\texport PYTHONPATH=\$PYTHONPATH:/usr/local/bin/"
+ln -s "$PWD"/python3/build/crypt.py /usr/local/bin/crypt.py
+
+if [ -f ~/.bashrc ]; then
+    echo "export PYTHONPATH=\$PYTHONPATH:/usr/local/bin/" >> ~/.bashrc
+    . ~/.bashrc
+else if [ -f ~/.bash_profile ]; then
+    echo "export PYTHONPATH=\$PYTHONPATH:/usr/local/bin/" >> ~/.bash_profile
+    . ~/.bash_profile
+else
+    export PYTHONPATH=$PYTHONPATH:/usr/local/bin/
+    echo -e "The location of the Python3 build tools has been added to your PYTHONPATH for this terminal session.\nHowever, it's highly recommended to put the following in one of your shell's login scripts:\n\n\texport PYTHONPATH=\$PYTHONPATH:/usr/local/bin/"
+fi
 
 echo
 echo "$(tput setaf 2)[INFO]$(tput sgr0) Installation complete."
