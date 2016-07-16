0f34c2c0a12e23d15c11ca98f39587e348c83639
Benjamin Toll <benjam72@yahoo.com>
2016-07-16 18:36:09 -0400

    Added password.c

---
+++ c/password.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 10
+
+int main (int argc, char *argv[])
+{
+    int size = argc > 1 ? atoi(argv[1]) : MAX;
+
+    char bucket[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_.,<>'/\\\"?;:!@#$%&*()-+[]^`=";
+    char generated[size + 1];
+    // Account for the ending nul character.
+    int len = sizeof(bucket) - 1;
+
+    srand(time(NULL));
+
+    for (int i = 0; i < size; i++) {
+//        *(generated + i) = bucket[(0 + rand() % len)];
+        generated[i] = bucket[(0 + rand() % len)];
+    }
+
+    generated[size] = '\0';
+    puts(generated);
+
+    return 0;
+}
+
