a01f44f0c61348ab9aec68f171671b616df2e27d
Benjamin Toll <benjam72@yahoo.com>
2016-08-15 21:49:02 -0400

    Made stat an async call to capture errors

--- node/server.js
+++ node/server.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 'use strict';
 
-const http = require('http');
-const fs = require('fs');
 const cp = require('child_process');
+const fs = require('fs');
+const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -13,57 +13,64 @@ const server = http.createServer((req, res) => {
 
     if (url.indexOf('favicon') === -1) {
         const currentLocation = `${process.cwd()}${url}`;
-        const stat = fs.statSync(currentLocation);
+        fs.stat(currentLocation, (err, stats) => {
+            if (err) {
+                // TODO: Capture more error codes.
+                if (err.errno === -2) {
+                    console.log(`404 NOT FOUND ${currentLocation}`);
+                }
+            } else {
+                if (stats.isFile()) {
+                  fs.readFile(currentLocation, (err, data) => {
+                      if (err) {
+                          console.log(`ERROR: ${err}`);
+                          res.statusCode = 500;
+                          res.end('Cannot display file');
+                      } else {
+                          const contentType = url.indexOf('htm') > -1 ?
+                              'text/html' :
+                              'text/plain';
 
-        if (stat.isFile()) {
-          fs.readFile(currentLocation, (err, data) => {
-              if (err) {
-                  console.log(`ERROR: ${err}`);
-                  res.statusCode = 500;
-                  res.end('Cannot display file');
-              } else {
-                  const contentType = url.indexOf('htm') > -1 ?
-                      'text/html' :
-                      'text/plain';
-
-                  res.setHeader('Content-Type', contentType);
-                  res.end(data);
-              }
-          });
-        } else {
-            fs.readdir(currentLocation, (err, files) => {
-                if (err) {
-                    console.log(`ERROR: ${err}`);
-                    res.statusCode = 500;
-                    res.end('Cannot display directory');
+                          res.setHeader('Content-Type', contentType);
+                          res.end(data);
+                      }
+                  });
                 } else {
-                    const entries = [];
-                    let html = [
-                        `<html><body><h1>Directory listing for ${url}</h1><ul>`
-                    ];
-
-                    res.statusCode = 200;
-                    res.setHeader('Content-Type', 'text/html');
-
-                    files.forEach(file => {
-                        const path = `${currentLocation}${file}` ;
-                        const stat = fs.statSync(path);
-
-                        if (stat.isDirectory()) {
-                            entries.push(`${file}/`);
+                    fs.readdir(currentLocation, (err, files) => {
+                        if (err) {
+                            console.log(`ERROR: ${err}`);
+                            res.statusCode = 500;
+                            res.end('Cannot display directory');
                         } else {
-                            entries.push(file);
+                            const entries = [];
+                            let html = [
+                                `<html><body><h1>Directory listing for ${url}</h1><ul>`
+                            ];
+
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type', 'text/html');
+
+                            files.forEach(file => {
+                                const path = `${currentLocation}${file}` ;
+                                const stat = fs.statSync(path);
+
+                                if (stat.isDirectory()) {
+                                    entries.push(`${file}/`);
+                                } else {
+                                    entries.push(file);
+                                }
+                            });
+
+                            entries.forEach(entry =>
+                                html.push(`<li><a href="${entry}">${entry}</a></li>`)
+                            );
+
+                            res.end(html.join(''));
                         }
                     });
-
-                    entries.forEach(entry =>
-                        html.push(`<li><a href="${entry}">${entry}</a></li>`)
-                    );
-
-                    res.end(html.join(''));
                 }
-            });
-        }
+            }
+        });
     }
 });
 
