63d5ec471f47a9d6fc35b8fe99f7c675f67e65fa
Benjamin Toll <benjam72@yahoo.com>
2019-01-19 13:38:12 -0500

    Renamed priv_key.c to modular_multiplicative_inverse.c

--- c/crypto/modular_multiplicative_inverse.c
+++ c/crypto/modular_multiplicative_inverse.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char **argv) {
     if (argc < 3) {
-        printf("Finds the modulus multiplicative inverse of a number `e`:\n\n");
+        printf("Finds the modulus multiplicative inverse(s) of a number `e`:\n\n");
         printf("\tde = 1 mod n\n\n");
         printf("Defaults to searching the entire key length `n`.\n");
         printf("Usage: %s <e> <mod> <bound>\n", argv[0]);
