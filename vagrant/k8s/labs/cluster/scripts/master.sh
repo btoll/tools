db4434171fe1da7d8d08e662943fdce6cd375951
Benjamin Toll <benjam72@yahoo.com>
2021-02-14 00:39:22 -0500

    Fix bugs

--- vagrant/k8s/labs/cluster/scripts/master.sh
+++ vagrant/k8s/labs/cluster/scripts/master.sh
@@ -9,7 +9,7 @@ echo "********** $MSG"
 echo "********** $MSG"
 echo "********** $MSG ->> Initializing Kubernetes Cluster"
 echo "********** $MSG ->> Master Node $NODE"
-echo "********** $MSG ->> kv-master-$NODE"
+echo "********** $MSG ->> bt-master-$NODE"
 kubeadm init --pod-network-cidr "$POD_CIDR" --apiserver-advertise-address "$API_ADV_ADDRESS" | tee /vagrant/kubeadm-init.out
 
 echo "********** $MSG"
@@ -20,8 +20,10 @@ echo "********** $MSG"
 mkdir -p /home/vagrant/.kube
 cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 chown vagrant:vagrant /home/vagrant/.kube/config
+
 mkdir -p /root/.kube
 cp -i /etc/kubernetes/admin.conf /root/.kube/config
+
 mkdir -p /vagrant/.kube
 cp -i /etc/kubernetes/admin.conf /vagrant/.kube/config
 
@@ -33,13 +35,32 @@ echo "********** $MSG ->> Downloading Calico YAML File"
 echo "********** $MSG"
 echo "********** $MSG"
 wget -q https://docs.projectcalico.org/v3.10/manifests/calico.yaml -O /tmp/calico-default.yaml
-#wget -q https://bit.ly/kv-lab-k8s-calico-yaml -O /tmp/calico-default.yaml
 sed "s+192.168.0.0/16+$POD_CIDR+g" /tmp/calico-default.yaml > /tmp/calico-defined.yaml
 
 echo "********** $MSG ->> Applying Calico YAML File"
 echo "********** $MSG"
 echo "********** $MSG"
 kubectl apply -f /tmp/calico-defined.yaml
+
+# The coredns Pod is only installed after a successful Pod network add-on (calico) install.
+#RES=$(awk '/coredns/' <<< "$(kubectl get pods -n kube-system)")
+#if [ -z "$RES" ]
+#then
+#    echo "********** $MSG"
+#    echo "********** $MSG ->> Calico Pod network add-on was not properly installed, aborting..."
+#    echo "********** $MSG"
+#    exit 1
+#elif [ "$(awk 'NR==1 { print $3 }' <<< "$RES")" != Running ]
+#then
+#    echo "********** $MSG"
+#    echo "********** $MSG ->> Calico Pod network add-on was installed but is not in the Running state, aborting..."
+#    echo "********** $MSG"
+#    exit 1
+#fi
+
 rm /tmp/calico-default.yaml /tmp/calico-defined.yaml
 echo "KUBELET_EXTRA_ARGS=--node-ip=10.8.8.1$NODE" > /etc/default/kubelet
+
+systemctl daemon-reload
+systemctl restart kubelet
 
--- vagrant/k8s/labs/cluster/scripts/worker.sh
+++ vagrant/k8s/labs/cluster/scripts/worker.sh
@@ -3,7 +3,7 @@
 
 MSG=$1
 NODE=$2
-NODE_HOST_IP=20+$NODE
+NODE_HOST_IP=$((20+"$NODE"))
 POD_CIDR=$3
 API_ADV_ADDRESS=$4
 
@@ -11,11 +11,14 @@ echo "********** $MSG"
 echo "********** $MSG"
 echo "********** $MSG ->> Joining Kubernetes Cluster"
 echo "********** $MSG ->> Worker Node $NODE"
-echo "********** $MSG ->> kv-worker-$NODE"
+echo "********** $MSG ->> bt-worker-$NODE"
 echo "********** $MSG ->> Pod CIDR $POD_CIDR"
 echo "********** $MSG ->> API address $API_ADV_ADDRESS"
 
 # Extract and execute the kubeadm join command from the exported file.
 $(< /vagrant/kubeadm-init.out grep -A 2 "kubeadm join" | sed -e 's/^[ \t]*//' | tr '\n' ' ' | sed -e 's/ \\ / /g')
 echo "KUBELET_EXTRA_ARGS=--node-ip=10.8.8.$NODE_HOST_IP" > /etc/default/kubelet
+
+systemctl daemon-reload
+systemctl restart kubelet
 
